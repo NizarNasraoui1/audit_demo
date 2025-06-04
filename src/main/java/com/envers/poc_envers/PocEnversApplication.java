01eec1322761a8e2fc810e782490ea2981f38a20
Nizar <nizar.al.nasraoui@gmail.com>
2025-06-04 12:45:27 +0200

    first commit

---
+++ src/main/java/com/envers/poc_envers/PocEnversApplication.java
@@ -0,0 +1,13 @@
+package com.envers.poc_envers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocEnversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocEnversApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/envers/poc_envers/PocEnversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.envers.poc_envers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocEnversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
